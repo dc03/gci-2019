5c42ff100f008170246d67d052117910a44b2116
dhruv-chawla <dhruv263.dc@gmail.com>
2019-12-18 18:21:40 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
-use std::io;
+use std::{io, process, io::Write};
+use morse::{encode, decode};
 
 fn main() -> std::io::Result<()> {
+    let mut type_enc_denc = String::new();
+    print!("1) Text -> morse (or)\n2) Morse -> text\n> ");
+    io::stdout().flush();
+    io::stdin().read_line(&mut type_enc_denc).expect("Could not read input");
+    let type_enc_denc = type_enc_denc.trim().parse::<u8>()
+        .unwrap_or_else(|err| {
+            println!("Error: {}", err);
+            process::exit(-1);
+        });
+    assert!(type_enc_denc == 1 || type_enc_denc == 2);
     println!("Enter the string to be converted: ");
     let mut user_input = String::new();
     io::stdin().read_line(&mut user_input).expect("Could not read input");
     user_input.make_ascii_uppercase();
-    let mut result = String::new();
+    if type_enc_denc == 1 {
+        match encode::encode(user_input.as_str()) {
+            Ok(output) => {
+                println!("Encoded string: {}", output);
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+            }
+        }
+    } else if type_enc_denc == 2 {
+        match decode::decode(user_input.as_str()) {
+            Ok(output) => {
+                println!("Decoded string: {}", output);
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+            }
+        }
+    }
+
+/*    let mut result = String::new();
     for elem in user_input.chars() {
         match elem {
             'A' => result.push_str(".-"),
@@ -47,6 +78,6 @@ fn main() -> std::io::Result<()> {
             other => result.push(other)
         }
     }
-    println!("Morse code is:\n{}", result);
+    println!("Morse code is:\n{}", result);*/
     Ok(())
 }
