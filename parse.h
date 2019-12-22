c0838f3ab3fe182ca102f57ed779bf555b3b60e1
dhruv-chawla <dhruv263.dc@gmail.com>
2019-12-22 11:11:47 +0530

    added api for units and base units

--- parse.h
+++ parse.h
@@ -9,6 +9,205 @@
 # ifndef __FILE_PARSE_H
 #   define __FILE_PARSE_H
 
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <cstring>
+#include <fstream>
+#include <stdexcept>
+#include <sstream>
+
+#include ".\units.h"
+
+auto parse_file(std::string&) -> bool;
+auto line_type(std::string&) -> int64_t;
+auto is_num(std::string&) -> bool;
+auto is_name(std::string&) -> bool;
+auto tokenise_base(std::string&, handlers::BaseHandler&) -> void;
+
+namespace handlers
+{
+    class BaseHandler
+    {
+        std::vector<std::shared_ptr<unit::BaseUnit>> m_base_units;
+
+    public:
+        BaseHandler() {}
+        BaseHandler(BaseHandler&) = delete;
+        BaseHandler& operator = (BaseHandler&) = delete;
+        auto add_base(std::string) -> void;
+        auto update_base(std::string, std::string) -> void;
+        auto get_base(std::string) -> std::shared_ptr<unit::BaseUnit>&;
+    };
+
+    class UnitHandler
+    {
+        std::vector<std::shared_ptr<unit::Unit>> m_units;
+
+    public:
+        UnitHandler() {}
+        UnitHandler(UnitHandler&) = delete;
+        UnitHandler& operator = (UnitHandler&) = delete;
+        auto add_unit(std::string, BaseHandler&, double, std::string) -> void;
+        auto update_unit(std::string, std::string) -> void;
+        auto get_unit(std::string) -> std::shared_ptr<unit::Unit>&;
+    };
+
+    auto BaseHandler::add_base(std::string unit_name) -> void
+    {
+        auto ptr = std::make_shared<unit::BaseUnit>(unit_name);
+        this -> m_base_units.push_back(ptr);
+    }
+
+    auto BaseHandler::update_base(std::string unit_name, std::string names)
+    -> void
+    {
+        for (auto &x: this -> m_base_units)
+            if (x -> get_name() == unit_name)
+            {
+                x -> set_names(names);
+                break;
+            }
+    }
+
+    auto BaseHandler::get_base(std::string unit_name) 
+    -> std::shared_ptr<unit::BaseUnit>&
+    {
+        for (auto &x: this -> m_base_units)
+            if (x -> get_name() == unit_name)
+                return x;
+            else
+                for (auto &names: x -> get_names())
+                    if (!std::strcmp(names.c_str(), unit_name.c_str()))
+                        return x;
+        throw std::invalid_argument{ "Base unit not found" }; // not found
+    }
+
+    auto UnitHandler::add_unit(std::string base_unit, BaseHandler &bases,
+        double value_in_base, std::string unit_name) -> void
+    {
+        try
+        {
+            auto base{ bases.get_base(base_unit) };
+            auto unit = std::make_shared<unit::Unit>(base, value_in_base,
+                unit_name);
+            this -> m_units.push_back(unit);
+        }
+        catch (const std::invalid_argument &e)
+        {
+            std::cerr << "Error: " << e.what() << "\nUnknown base unit: "
+            << base_unit;
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+
+    auto UnitHandler::update_unit(std::string unit_name, std::string names)
+        -> void
+    {
+        for (auto &x: this -> m_units)
+            if (x -> get_name() == names)
+            {
+                x -> set_names(names);
+                break;
+            }
+    }
+
+    auto UnitHandler::get_unit(std::string unit_name) 
+    -> std::shared_ptr<unit::Unit>&
+    {
+        for (auto &x: this -> m_units)
+            if (x -> get_name() == unit_name)
+                return x;
+            else
+                for (auto &name: x -> get_names())
+                    if (name == unit_name)
+                        return x;
+        throw std::invalid_argument{ "Unit not found" };
+    }
+};
+
+auto parse_file(std::string &file_name) -> bool
+{
+    std::ifstream fin(file_name.c_str(), std::ios::in);
+    handlers::BaseHandler bases;
+    handlers::UnitHandler units;
+    auto line_number{ 1 };
+    while (!fin.eof())
+    {
+        std::string line{};
+        std::getline(fin, line);
+        switch (line_type(line))
+        {
+            case 0: // comment
+            case 1: // base unit definition
+                try
+                { tokenise_base(line, bases); }
+                catch (const std::invalid_argument &e)
+                {
+                    std::cerr << "Error: " << e.what() << '\n' << file_name 
+                    << ":" << line_number << ": " << line 
+                    << "\nSyntax must be \"base\" \"unit\" <unit_name>";
+                }
+                catch (const std::exception &e)
+                { std::cerr << "Error: " << e.what(); }
+            case 2: // unit name definitions
+            case 3: // unit values definition
+            default: // anything else
+        }
+        line_number += 1;
+    }
+}
+
+auto line_type(std::string &line) -> int64_t
+{
+    std::stringstream stream{ line };
+    std::string token;
+    stream >> token;
+    if (!std::strcmp(token.c_str(), "#"))
+        return 0;
+    else if (!std::strcmp(token.c_str(), "base"))
+        return 1;
+    else if (is_num(token))
+        return 3;
+    else if (is_name(token))
+        return 2;
+    else
+        return -1;
+}
+
+auto is_num(std::string &value) -> bool
+{
+    for (const auto &x: value)
+        if (x < '0' || x > '9')
+            return false;
+    return true;
+}
+
+auto is_name(std::string &name) -> bool
+{
+    for (const auto &x: name)
+        if (x >= '0' && x <= '9')
+            return false;
+    return true;
+}
+
+auto tokenise_base(std::string &line, handlers::BaseHandler &base_units)
+    -> void
+{
+    std::stringstream stream{ line };
+    std::string base, unit, unit_name;
+    stream >> base >> unit >> unit_name;
+    if (!std::strcmp(base.c_str(), "base") && 
+        !std::strcmp(unit.c_str(), "unit"))
+        base_units.add_base(unit_name);
+    else
+        throw std::invalid_argument{ "Wrong line syntax" };
+}
+
 # endif
 
 # endif
--- units.h
+++ units.h
@@ -7,43 +7,47 @@
 # else
 
 #include <bits/stdc++.h>
-#include <fstream>
 #include <cstring>
 #include <cstdio>
 #include <vector>
-#include <algorithm>
 #include <memory>
+#include <stdexcept>
 
 # ifndef __UNITS_H
 #   define __UNITS_H
 
 namespace unit
 {
-    class BaseUnit;
-    class Unit;
-
     class BaseUnit
     {
+        std::string m_name{};
         std::vector<std::string> m_names{};
     public:
         BaseUnit(std::string);
+        auto set_names(std::string) -> void;
+        auto get_name() -> std::string;
         auto get_names() -> std::vector<std::string>;
     };
 
     class Unit
     {
-        BaseUnit *m_base_unit{ nullptr };
+        std::string m_name{};
+        std::shared_ptr<BaseUnit> m_base_unit{ nullptr };
         double m_value_in_base{ 0 };
         std::vector<std::string> m_names{};
     public:
-        Unit(BaseUnit*, double, std::string);
+        Unit(std::shared_ptr<BaseUnit>, double, std::string);
         auto try_into(Unit &other) -> bool;
-        auto get_base() -> BaseUnit*;
+        auto get_base() -> std::shared_ptr<BaseUnit>;
         auto get_val_as_base() -> double;
+        auto set_names(std::string) -> void;
+        auto get_name() -> std::string;
         auto get_names() -> std::vector<std::string>;
     };
 
-    BaseUnit::BaseUnit(std::string names)
+    BaseUnit::BaseUnit(std::string name): m_name{ name } {}
+
+    auto BaseUnit::set_names(std::string names) -> void
     {
         auto names_str = new char[names.length()];
         std::strcpy(names_str, names.c_str());
@@ -55,12 +59,24 @@ namespace unit
         }
         delete[] names_str;
     }
+
+    auto BaseUnit::get_name() -> std::string
+    { return this -> m_name; }
 
     auto BaseUnit::get_names() -> std::vector<std::string>
     { return this -> m_names; }
 
-    Unit::Unit(BaseUnit *base, double value_base, std::string names): 
-        m_base_unit{ base }, m_value_in_base{ value_base } 
+    Unit::Unit(std::shared_ptr<BaseUnit> base, double value_base, 
+        std::string name): m_base_unit{ base }, m_value_in_base{ value_base },
+                           m_name{ name } {}
+
+    auto Unit::get_base() -> std::shared_ptr<BaseUnit>
+    { return this -> m_base_unit; }
+
+    auto Unit::get_val_as_base() -> double
+    { return this -> m_value_in_base; }
+
+    auto Unit::set_names(std::string names) -> void
     {
         auto names_str = new char[names.length()];
         std::strcpy(names_str, names.c_str());
@@ -73,11 +89,8 @@ namespace unit
         delete[] names_str;
     }
 
-    auto Unit::get_base() -> BaseUnit*
-    { return this -> m_base_unit; }
-
-    auto Unit::get_val_as_base() -> double
-    { return this -> m_value_in_base; }
+    auto Unit::get_name() -> std::string
+    { return this -> m_name; }
 
     auto Unit::get_names() -> std::vector<std::string>
     { return this -> m_names; }
